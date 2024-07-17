dbf7806991311dd2a8958efe4aee399d59bfec43
Kuniyuki Iwashima <kuniyu@amazon.com>
2024-07-17 16:58:04 +0000

    env: Add bashrc target.
    `make bashrc` updates ~/.bashrc with snippets under env/bashrc/snippet.
    
    Note that idempotence is guaranteed as with emacs target.
    
    Signed-off-by: Kuniyuki Iwashima <kuniyu@amazon.com>

---
+++ env/dev/bashrc/snippet/grep.sh
@@ -0,0 +1,3 @@
+alias cgrep="grep -rn --include=*.c --exclude=*.mod.c"
+alias hgrep="grep -rn --include=*.h"
+alias chgrep="grep -rn --include=*.c --include=*.h --exclude=*.mod.c"
---
+++ env/dev/bashrc/snippet/make.sh
@@ -0,0 +1,1 @@
+alias jake="make -j $(($(nproc) * 2))"
